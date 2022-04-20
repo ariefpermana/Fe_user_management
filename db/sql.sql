74d400abe98dbf3165883adbc957a5d1fbfafe99
ariefpermana <ariefpermana93@gmail.com>
2022-04-21 06:59:34 +0700

    update sql file

--- db/sql.sql
+++ db/sql.sql
@@ -15,3 +15,11 @@ CREATE TABLE public.tb_master_user (
 	role_id int4 NULL,
 	CONSTRAINT tb_master_user_pkey PRIMARY KEY (user_id)
 );
+
+INSERT INTO public.tb_master_user (alamat,created_on,email,jenis_kelamin,lastupdated,"name",role_id) VALUES
+	 ('tangerang','2022-04-21 09:02:31','ariefpermana@gmail.com','L','2022-04-21 13:12:15','arief permana',0);
+
+INSERT INTO public.tb_master_user_role (role_id,role_name) VALUES
+	 (0,'Admin'),
+	 (1,'Developer'),
+	 (2,'QA');
